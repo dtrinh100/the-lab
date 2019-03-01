fd0406acd5a56f3991aef11040e3ab5b0981b5a0
dtrinh100 <dtrinh100@gmail.com>
2019-03-01 01:29:55 -0500

    Updating tests for ProductSearch

--- tests/unit/components/ProductSearch.spec.js
+++ tests/unit/components/ProductSearch.spec.js
@@ -1,9 +1,12 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import ProductSearch from "@/components/ProductSearch.vue";
 import Vuex from "vuex";
+import VueRouter from "vue-router";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
+const router = new VueRouter();
 
 describe("ProductSearch", () => {
   let store;
@@ -39,29 +42,33 @@ describe("ProductSearch", () => {
       }
     });
 
-    wrapper = shallowMount(
-      ProductSearch,
-      {
-        propsData: {
-          reset: false
-        },
-        computed: {
-          keyword: () => ""
-        },
-        stubs: ["font-awesome-icon"],
-        store
+    wrapper = shallowMount(ProductSearch, {
+      propsData: {
+        reset: false
       },
-      localVue
-    );
+      computed: {
+        keyword: () => ""
+      },
+      stubs: ["font-awesome-icon"],
+      store,
+      localVue,
+      router
+    });
   });
 
   describe("ProductSearch User Interface", () => {
     it("has an input field for searching", () => {
       expect(wrapper.contains("input")).toBe(true);
     });
-
     it("has an button for searching", () => {
       expect(wrapper.contains("button")).toBe(true);
     });
+    it("calls the getKeyword action when an input event is triggered", () => {
+      const input = wrapper.find("input");
+      input.element.value = "testing";
+      input.trigger("input");
+      expect(actions.getKeyword).toHaveBeenCalled();
+    });
+    it("should push you to a new route when you click the button", () => {});
   });
 });
