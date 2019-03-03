a33e6695a1d767573c9a1f0d28420680db6fb899
dtrinh100 <dtrinh100@gmail.com>
2019-03-03 00:27:33 -0500

    Starting on ProductInformation

---
+++ src/utils/api/EventsAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = `https://app.ticketmaster.com/discovery/v2/events`;
+export default {
+  getEvent(id, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${API}/${id}`, {
+          params
+        })
+        .then(response => resolve(response.data))
+        .catch(e => reject(e));
+    });
+  }
+};
--- src/views/ProductInformation.vue
+++ src/views/ProductInformation.vue
@@ -3,12 +3,17 @@
 </template>
 
 <script>
+import eventsAPI from "@/utils/api/EventsAPI";
+import BaseEvents from "@/models/BaseEvents";
+
 export default {
   name: "ProductInformation",
   data() {
     return {
       loading: false,
-      error: false
+      error: false,
+      eventRequest: null,
+      result: null
     };
   },
   created: function() {
@@ -22,9 +27,34 @@ export default {
   methods: {
     fetchData: async function(id) {
       if (!id) {
+        // If we are missing an id for some reason, this is an error and should be shown as such
+        this.error = true;
         return;
       }
       this.loading = true;
+
+      const params = {
+        apikey: process.env.VUE_APP_KEY
+      };
+
+      this.eventRequest = eventsAPI.getEvent(id, params);
+
+      const data = await this.eventRequest.catch(err => {
+        this.loading = false;
+        this.error = true;
+        console.log(err);
+      });
+
+      this.loading = false;
+
+      this.parseData(data);
+    },
+    parseData: function(data) {
+      const event = Object.create(BaseEvents);
+      event.populate(data);
+
+      this.result = event;
+      console.log(this.result);
     }
   }
 };
