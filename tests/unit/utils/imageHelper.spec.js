b3cb57ef3cc89d697d0698ee3b973c84982824ae
dtrinh100 <dtrinh100@gmail.com>
2019-03-03 22:33:26 -0500

    Update unit test wording

--- tests/unit/utils/imageHelper.spec.js
+++ tests/unit/utils/imageHelper.spec.js
@@ -10,12 +10,12 @@ describe("imageHelper", () => {
     { url: "testing3.jpg", height: 223 }
   ];
 
-  it("should select the right image object from the image array", async () => {
+  it("should return an image object that matches the given image height parameter", async () => {
     const expectedResult = { url: "testing2.jpg", height: 1532 };
     const result = imageHelper(mockImageArray, 1532);
     expect(result).toEqual(expectedResult);
   });
-  it("should return an empty array if the image height parameters doesn't match anything in the image array", async () => {
+  it("should return an empty object if the image height parameters doesn't match anything in the image array", async () => {
     const expectedResult = {};
     const result = imageHelper(mockImageArray, 98);
     expect(result).toEqual(expectedResult);
